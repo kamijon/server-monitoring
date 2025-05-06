862c521a86eeeb577ce708c38d2171947e914bfe
kamijon <its.company@gmail.com>
2025-05-06 17:45:49 +0330

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,7 +1,10 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from app.database import Base, engine
 
-# Create all tables
 Base.metadata.create_all(bind=engine)
 
 print("✅ Database and tables created successfully.")
-
